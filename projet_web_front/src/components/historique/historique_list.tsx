123fad8e4c3022880d400decbbb5aa578bf05357
Lora Seguin <lora80123@gmail.com>
2025-03-06 15:36:46 +0100

    feat(smartsecure/webui): add page historic - #8

---
+++ projet_web_front/src/components/historique/historique_list.tsx
@@ -0,0 +1,19 @@
+import { AppSidebar } from "../sidebar/app-sidebar";
+import { SidebarProvider } from "../ui/sidebar";
+import { HistoriqueTable } from "./historique_table";
+
+export function ListHistorique() {
+    return (
+        <div className="h-screen relative flex overflow-hidden">
+            <SidebarProvider>
+                <AppSidebar />
+            </SidebarProvider>
+            <main className="h-full w-full flex-auto overflow-auto antialiased">
+                <p className="text-5xl text-center">Historique</p>
+                <div className="p-5">
+                    <HistoriqueTable />
+                </div>
+            </main>
+        </div>
+    );
+}
